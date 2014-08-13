fdd2e2a05a1f7e824e8f77e43b7ce785e44367f9
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-08-14 00:02:55 +0200

    Some simplification on the code provided by the template.

--- FilmBookmarkService/Controllers/AccountController.cs
+++ FilmBookmarkService/Controllers/AccountController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -21,7 +18,7 @@ namespace FilmBookmarkService.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -77,7 +74,7 @@ namespace FilmBookmarkService.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return _RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -123,11 +120,11 @@ namespace FilmBookmarkService.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(model.ReturnUrl);
+                    return _RedirectToLocal(model.ReturnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.Failure:
@@ -158,8 +155,8 @@ namespace FilmBookmarkService.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
@@ -168,7 +165,7 @@ namespace FilmBookmarkService.Controllers
 
                     return RedirectToAction("Index", "Home");
                 }
-                AddErrors(result);
+                _AddErrors(result);
             }
 
             // If we got this far, something failed, redisplay form
@@ -262,7 +259,7 @@ namespace FilmBookmarkService.Controllers
             {
                 return RedirectToAction("ResetPasswordConfirmation", "Account");
             }
-            AddErrors(result);
+            _AddErrors(result);
             return View();
         }
 
@@ -336,7 +333,7 @@ namespace FilmBookmarkService.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return _RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -378,10 +375,10 @@ namespace FilmBookmarkService.Controllers
                     if (result.Succeeded)
                     {
                         await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-                        return RedirectToLocal(returnUrl);
+                        return _RedirectToLocal(returnUrl);
                     }
                 }
-                AddErrors(result);
+                _AddErrors(result);
             }
 
             ViewBag.ReturnUrl = returnUrl;
@@ -408,7 +405,7 @@ namespace FilmBookmarkService.Controllers
 
         #region Helpers
         // Used for XSRF protection when adding external logins
-        private const string XsrfKey = "XsrfId";
+        private const string XSRF_KEY = "XsrfId";
 
         private IAuthenticationManager AuthenticationManager
         {
@@ -418,7 +415,7 @@ namespace FilmBookmarkService.Controllers
             }
         }
 
-        private void AddErrors(IdentityResult result)
+        private void _AddErrors(IdentityResult result)
         {
             foreach (var error in result.Errors)
             {
@@ -426,12 +423,11 @@ namespace FilmBookmarkService.Controllers
             }
         }
 
-        private ActionResult RedirectToLocal(string returnUrl)
+        private ActionResult _RedirectToLocal(string returnUrl)
         {
             if (Url.IsLocalUrl(returnUrl))
-            {
                 return Redirect(returnUrl);
-            }
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -458,11 +454,12 @@ namespace FilmBookmarkService.Controllers
                 var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
                 if (UserId != null)
                 {
-                    properties.Dictionary[XsrfKey] = UserId;
+                    properties.Dictionary[XSRF_KEY] = UserId;
                 }
                 context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
             }
         }
+
         #endregion
     }
 }
