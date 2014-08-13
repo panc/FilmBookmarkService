506730ff74f03ec16cb6ed516e152edd6222ea32
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-08-13 17:33:16 +0200

    Add solution.

---
+++ FilmBookmarkService/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FilmBookmarkService.Startup))]
+
+namespace FilmBookmarkService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
