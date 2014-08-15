f54cfaa852574b3a3302322bd19a792811ba535a
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-08-15 07:47:41 +0200

    Started workin on film controller.

---
+++ FilmBookmarkService/Controllers/FilmController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using FilmBookmarkService.Models;
+using WebGrease;
+
+namespace FilmBookmarkService.Controllers
+{
+    public class FilmController : Controller
+    {
+        public ActionResult Index()
+        {
+            var films = new List<FilmModel>
+            {
+                new FilmModel { Name = "Hugo" },
+                new FilmModel { Name = "Sep" },
+                new FilmModel { Name = "Ich" }
+            };
+
+            return View(films);
+        }
+    }
+}
---
+++ FilmBookmarkService/Models/FilmModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FilmBookmarkService.Models
+{
+    public class FilmModel
+    {
+        public string Name { get; set; }
+
+        public string Link { get; set; }
+
+        public string Stream { get; set; }
+
+        public string Season { get; set; }
+        
+        public string Episode { get; set; } 
+    }
+}
