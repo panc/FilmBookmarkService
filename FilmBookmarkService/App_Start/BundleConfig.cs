bee7306f2995026044138a0de6ca289e1d905351
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-08-15 08:21:35 +0200

    Added js support to film view.

--- FilmBookmarkService/App_Start/BundleConfig.cs
+++ FilmBookmarkService/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace FilmBookmarkService
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/fbs").Include(
+                      "~/Scripts/fbs.film.js")); 
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
