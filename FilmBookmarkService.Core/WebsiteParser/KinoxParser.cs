349298533764638124c509377bf6a5e4af2c4611
Christoph Pangerl <christoph.pangerl@gmail.com>
2014-10-03 21:00:53 +0200

    Added support for new domains of kinox.

--- FilmBookmarkService.Core/WebsiteParser/KinoxParser.cs
+++ FilmBookmarkService.Core/WebsiteParser/KinoxParser.cs
@@ -11,45 +11,47 @@ namespace FilmBookmarkService.Core
     public class KinoxParser : IWebsiteParser
     {
         // Sample URL
-        // http://kinox.to/Stream/Castle.html
+        // http://kinox.tv/Stream/Castle.html
 
         private const string HOSTER_STREAMCLOUD = "30";
-        private const string GET_MIRROR_URL = "http://kinox.to/aGET/Mirror/{0}&Hoster={1}&Season={2}&Episode={3}&Mirror={4}";
-        private const string BASE_URL = "kinox.to/Stream/";
+        private const string GET_MIRROR_URL = "http://kinox.tv/aGET/Mirror/{0}&Hoster={1}&Season={2}&Episode={3}&Mirror={4}";
+        private const string LOCKED_BASE_URL = "kinox.to";
+        private const string BASE_URL = "kinox.tv";
+        private const string URL_TEMPLATE = "kinox.tv/Stream/";
 
         public Task<bool> IsCompatible(string url)
         {
             return Task.Factory.StartNew(() =>
             {
-                url = _PrepareBaseUrl(url);
-                return !string.IsNullOrEmpty(url) && url.StartsWith(BASE_URL);
+                url = _PrepareUrl(url);
+                url = url.Replace("http://www.", "")
+                         .Replace("http://", "");
+
+                return !string.IsNullOrEmpty(url) && url.StartsWith(URL_TEMPLATE);
             });
         }
 
         public async Task<string> GetCoverUrl(string filmUrl)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(new Uri(filmUrl));
-            response.EnsureSuccessStatusCode();
-
-            var content = await response.Content.ReadAsStringAsync();
+            var content = await _ExecuteHttpRequest(filmUrl);
 
             var doc = new HtmlDocument();
             doc.LoadHtml(content);
             var coverNode = doc.DocumentNode.SelectSingleNode("//div[@class='Grahpics']//a//img[@src]");
 
-            return coverNode != null
+            var src = coverNode != null
                 ? coverNode.GetAttributeValue("src", "")
                 : string.Empty;
+
+            if (string.IsNullOrEmpty(src))
+                return string.Empty;
+
+            return _PrepareUrl(src);
         }
 
         public async Task<int> GetNumberOfEpisodes(string filmUrl, int season)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(new Uri(filmUrl));
-            response.EnsureSuccessStatusCode();
-
-            var content = await response.Content.ReadAsStringAsync();
+            var content = await _ExecuteHttpRequest(filmUrl);
 
             var doc = new HtmlDocument();
             doc.LoadHtml(content);
@@ -107,11 +109,7 @@ namespace FilmBookmarkService.Core
 
         public async Task<bool> IsAnotherEpisodeAvailable(string filmUrl, int season, int episode)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(new Uri(filmUrl));
-            response.EnsureSuccessStatusCode();
-
-            var content = await response.Content.ReadAsStringAsync();
+            var content = await _ExecuteHttpRequest(filmUrl);
 
             var doc = new HtmlDocument();
             doc.LoadHtml(content);
@@ -144,14 +142,10 @@ namespace FilmBookmarkService.Core
             // the the service returns the default mirror (number 1).
             var mirrorNumber = new Random().Next(1, 5);
 
-            var filmId = _ParseUrlForFilmId(filmUrl);
+            var filmId = await _ParseUrlForFilmId(filmUrl);
             var url = string.Format(GET_MIRROR_URL, filmId, hoster, season, episode, mirrorNumber);
 
-            var client = new HttpClient();
-            var response = await client.GetAsync(new Uri(url));
-            response.EnsureSuccessStatusCode();
-
-            var content = await response.Content.ReadAsStringAsync();
+            var content = await _ExecuteHttpRequest(url);
             var mirror = JsonConvert.DeserializeObject<KinoxMirrorDto>(content);
 
             if (mirror == null)
@@ -164,19 +158,31 @@ namespace FilmBookmarkService.Core
             return node.Attributes["href"].Value;
         }
 
-        private string _ParseUrlForFilmId(string url)
+        private async Task<string> _ParseUrlForFilmId(string url)
         {
-            url = _PrepareBaseUrl(url);
-
-            if (string.IsNullOrEmpty(url) || !url.StartsWith(BASE_URL))
+            if (!await IsCompatible(url))
                 return string.Empty;
 
-            return url.Replace(BASE_URL, "").Replace(".html", "");
+            url = url.Replace("http://www.", "")
+                     .Replace("http://", "");
+
+            return url.Replace(URL_TEMPLATE, "").Replace(".html", "");
         }
 
-        private string _PrepareBaseUrl(string url)
+        private string _PrepareUrl(string url)
         {
-            return url.Replace("http://www.", "").Replace("http://", "");
+            return url.Replace(LOCKED_BASE_URL, BASE_URL);
+        }
+
+        private async Task<string> _ExecuteHttpRequest(string filmUrl)
+        {
+            var url = _PrepareUrl(filmUrl);
+            var client = new HttpClient();
+
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
